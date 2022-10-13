1cf0272ae2756f5ee66997d1c616aba579ec4cde
Abhishek Kumar <abhishek.kumar.ug20@nsut.ac.in>
2022-10-13 04:50:32 -0700

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDgLBHmLD2l6N8KRqMY6ESL62Gy92T5bHo",
+  authDomain: "crypto-hunter-93428.firebaseapp.com",
+  projectId: "crypto-hunter-93428",
+  storageBucket: "crypto-hunter-93428.appspot.com",
+  messagingSenderId: "918413561024",
+  appId: "1:918413561024:web:449428b97ff9beb9fc3125"
+};
+
+export default firebaseConfig;
